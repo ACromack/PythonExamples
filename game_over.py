0c7813cfaffbedf5a9fd127fc3a085dab1b78393
Ashley Cromack <Cromazone@live.co.uk>
2018-03-14 08:58:10 +0000

    Added Python Apps
    Added two simple Python applications for displaying text and times tables.

---
+++ game_over.py
@@ -0,0 +1,9 @@
+# Game Over - Version 2
+# Demonstrates the use of quotes in strings
+
+print("Program 'Game Over' 2.0")
+
+print("Same", "message", "as before")
+
+
+print("Game Over")
---
+++ timesTable.py
@@ -0,0 +1,8 @@
+# Times Tables
+
+# Ask the user to input the number they would like the times tables for
+tTable = int(input("What number would you like to see the times table for? "))
+
+# Loop through 12 times
+for number in range(12):
+    print("{0} times {1} equals {2}" .format(number+1, tTable, (number+1) * tTable))
